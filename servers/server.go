dd10cb2fc54303c6e4eb6a43cf7b00c00e293492
Avyukth <makur.subhrajit@gmail.com>
2023-07-09 16:06:29 +0530

    server working with mongo

--- servers/server.go
+++ servers/server.go
@@ -5,39 +5,136 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	gamestats "github.com/Avyukth/lila-assgnm/api/proto"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
+	"gopkg.in/mgo.v2/bson"
 )
 
-
-
+// var db *mongo.Database
 type server struct {
 	gamestats.UnimplementedGameStatsServer
+	Mongo *MongoConnection
 }
 
+type gameModes struct {
+	BattleRoyal     int32 `bson:"battle_royal"`
+	TeamDeathMatch  int32 `bson:"team_death_match"`
+	CaptureTheFlag  int32 `bson:"capture_the_flag"`
+}
+
+type gameStat struct {
+	AreaCode  int32     `bson:"area_code"`
+	GameModes gameModes `bson:"game_modes"`
+}
+
+
 func (s *server) GetGameStats(ctx context.Context, in *gamestats.GameStatsRequest) (*gamestats.GameStatsResponse, error) {
-	// Static data for example
+	filter := bson.M{"area_code": in.AreaCode}
+	gameStatsCollection := s.Mongo.Db.Collection("gameStats")
+	cursor, err := gameStatsCollection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var gameModes []*gamestats.GameMode
+	for cursor.Next(ctx) {
+		var result gameStat
+		err := cursor.Decode(&result)
+		if err != nil {
+			return nil, err
+		}
+
+		gameModes = append(gameModes,
+			&gamestats.GameMode{Mode: "Battle Royale", Players: result.GameModes.BattleRoyal},
+			&gamestats.GameMode{Mode: "Team Death Match", Players: result.GameModes.TeamDeathMatch},
+			&gamestats.GameMode{Mode: "Capture The Flag", Players: result.GameModes.CaptureTheFlag},
+		)
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
 	return &gamestats.GameStatsResponse{
-		AreaCode: in.AreaCode, // Echo back the requested area code
-		GameModes: []*gamestats.GameMode{
-			&gamestats.GameMode{Mode: "Battle Royale", Players: 10},
-			&gamestats.GameMode{Mode: "Team Death Match", Players: 20},
-			&gamestats.GameMode{Mode: "Capture the Flag", Players: 30},
-		},
+		AreaCode:  in.AreaCode,
+		GameModes: gameModes,
 	}, nil
 }
 
 
-func main() {
-	lis, err := net.Listen("tcp", ":50051")
+
+type MongoConnection struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+
+type MongoConfig struct {
+	Uri        string
+	Database   string
+	MaxPoolSize uint64
+}
+
+func NewMongoConnection(cfg MongoConfig) (*MongoConnection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(cfg.Uri)
+	clientOptions.SetMaxPoolSize(cfg.MaxPoolSize)
+
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return nil, err
 	}
-	s := grpc.NewServer()
-	gamestats.RegisterGameStatsServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to MongoDB")
+
+	db := client.Database(cfg.Database)
+	return &MongoConnection{Client: client, Db: db}, nil
+}
+
+// Close closes the MongoDB connection.
+func (mc *MongoConnection) Close() {
+	err := mc.Client.Disconnect(context.Background())
+	if err != nil {
+		log.Println("Error disconnecting from MongoDB:", err)
+	}
+}
+
+func main() {
+	// connect to MongoDB
+	cfg := MongoConfig{
+		Uri:        "mongodb://root:example@localhost:27017/?directConnection=true",
+		Database:   "gameDB",
+		MaxPoolSize: 50,
+	}
+
+	mongoConn, err := NewMongoConnection(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	defer mongoConn.Close()
+	// start the gRPC server
+	grpcServer := grpc.NewServer()
+	gamestats.RegisterGameStatsServer(grpcServer, &server{Mongo: mongoConn})
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
 	}
 	os.Exit(0)
 }
